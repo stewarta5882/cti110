b627ed2587582d154b5e070876130b6ff6e79118
stewarta5882 <36607803+stewarta5882@users.noreply.github.com>
2018-02-18 15:30:24 -0500

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_StewartAyesha.py
@@ -0,0 +1,12 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Ayesha Stewart
+# 2/18/18
+#
+
+total_sales=float(input("Enter total sales: "))
+
+profit=total_sales*0.23
+
+print("Your profit will be $", profit)                  
+                  
